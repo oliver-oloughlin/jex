516a2039c0b855fdec2ee73ad48d7714d786d0fc
Oliver O'Loughlin <oliveroloughlin@hotmail.com>
2024-10-05 16:43:43 +0200

    chore: added tests and restructured utils

--- src/jex.ts
+++ src/jex.ts
@@ -1,21 +1,23 @@
 import { HttpError } from "./errors.ts"
 import type {
   ActionConfig,
-  BodyfullActionConfig,
   Client,
   ClientConfig,
   EndpointConfig,
   EndpointRecord,
   Fetcher,
   Method,
-  Plugin,
-  PluginAfterContext,
-  PluginBeforeContext,
   PossibleActionArgs,
   Result,
 } from "./types.ts"
-import { deepMerge } from "@std/collections"
 import { ulid } from "@std/ulid"
+import { createInitAndUrl, parseData, sendRequest } from "./utils.ts"
+
+/**************/
+/*            */
+/*   PUBLIC   */
+/*            */
+/**************/
 
 export function jex<
   const TEndpointRecord extends EndpointRecord<TFetcher>,
@@ -34,6 +36,12 @@ export function jex<
     TFetcher
   >
 }
+
+/***************/
+/*             */
+/*   PRIVATE   */
+/*             */
+/***************/
 
 function createEndpoint(
   path: string,
@@ -119,336 +127,4 @@ function createAction(
       }
     }
   }
-}
-
-function createUrl(
-  path: string,
-  clientConfig: ClientConfig<any, Fetcher>,
-  actionConfig: ActionConfig<any>,
-  args: PossibleActionArgs | undefined,
-) {
-  const baseUrl = clientConfig.baseUrl ?? "/"
-  const query = args?.query
-  const params = args?.params
-
-  let url = baseUrl
-    .replace(/[/]{1}$/, "")
-    .concat("/")
-    .concat(
-      path
-        .replace(/^[/]{1}/, "")
-        .replace(/[/]{1}$/, ""),
-    )
-
-  if (params) {
-    Object
-      .entries(params)
-      .forEach(([name, value]) => {
-        url = url.replace(`:${name}`, value.toString())
-        url = url.replace(`[${name}]`, value.toString())
-        url = url.replace(`{${name}}`, value.toString())
-        url = url.replace(`<${name}>`, value.toString())
-      })
-  }
-
-  if (query || actionConfig.query) {
-    const parsed = actionConfig.query?._transform?.(query ?? {}) ??
-      actionConfig.query?.parse(query ?? {}) ??
-      query
-
-    Object
-      .entries(parsed as Record<string, any>)
-      .forEach(([key, value]) => {
-        url = appendQuery(url, key, value)
-      })
-  }
-
-  return url
-}
-
-async function createInitAndUrl(
-  path: string,
-  clientConfig: ClientConfig<any, any>,
-  endpointConfig: EndpointConfig<any>,
-  actionConfig: ActionConfig<any>,
-  args: PossibleActionArgs | undefined,
-  method: Method,
-  id: string,
-) {
-  let url = createUrl(
-    path,
-    clientConfig,
-    actionConfig,
-    args,
-  )
-
-  const { body, contentType } = await createBody(actionConfig, args)
-  let init = {
-    body,
-    ...(contentType
-      ? {
-        headers: {
-          "Content-Type": contentType,
-        },
-      }
-      : {}),
-  } as object
-
-  let ctx: PluginBeforeContext<Fetcher> = {
-    id,
-    client: clientConfig,
-    endpoint: endpointConfig,
-    action: actionConfig,
-    url,
-    method,
-    init: init,
-    args: args ?? {},
-  }
-
-  for (const plugin of clientConfig.plugins ?? []) {
-    ctx = await applyBefore(ctx, plugin)
-  }
-
-  for (const plugin of endpointConfig.plugins ?? []) {
-    ctx = await applyBefore(ctx, plugin)
-  }
-
-  for (const plugin of actionConfig.plugins ?? []) {
-    ctx = await applyBefore(ctx, plugin)
-  }
-
-  const parsedHeaders =
-    actionConfig.headers?._transform?.(args?.headers ?? {}) ??
-      actionConfig.headers?.parse(args?.headers ?? {}) ??
-      args?.headers ?? {}
-
-  const stringifiedHeaders = stringifyEntries(parsedHeaders)
-  init = deepMerge(ctx.init as object, args?.init ?? {} as object)
-  init = deepMerge(init as object, { headers: stringifiedHeaders })
-  init = deepMerge(init as object, { method })
-
-  Object.entries(ctx.args?.query ?? {}).forEach(([key, value]) => {
-    url = appendQuery(url, key, value)
-  })
-
-  return {
-    init,
-    url,
-  }
-}
-
-async function sendRequest(
-  clientConfig: ClientConfig<any, any>,
-  endpointConfig: EndpointConfig<any>,
-  actionConfig: ActionConfig<any>,
-  args: PossibleActionArgs | undefined,
-  init: RequestInit,
-  url: string,
-  method: Method,
-  id: string,
-): Promise<Response> {
-  const fetcher = clientConfig.fetcher ?? fetch
-  let res = await fetcher(url, init)
-
-  let ctx: PluginAfterContext<Fetcher> = {
-    id,
-    client: clientConfig,
-    endpoint: endpointConfig,
-    action: actionConfig,
-    init,
-    args: args ?? {},
-    url,
-    method,
-    res,
-    async refetch(refetchInit) {
-      let i = deepMerge(init, refetchInit ?? {})
-      i = deepMerge(i, { method })
-      return await fetcher(url, init)
-    },
-  }
-
-  for (const plugin of clientConfig.plugins ?? []) {
-    ctx = {
-      ...ctx,
-      res,
-    }
-    res = await applyAfter(ctx, plugin)
-  }
-
-  for (const plugin of endpointConfig.plugins ?? []) {
-    ctx = {
-      ...ctx,
-      res,
-    }
-    res = await applyAfter(ctx, plugin)
-  }
-
-  for (const plugin of actionConfig.plugins ?? []) {
-    ctx = {
-      ...ctx,
-      res,
-    }
-    res = await applyAfter(ctx, plugin)
-  }
-
-  return res
-}
-
-async function applyBefore(
-  ctx: PluginBeforeContext<Fetcher>,
-  plugin: Plugin<Fetcher>,
-): Promise<PluginBeforeContext<Fetcher>> {
-  if (!plugin.before) return ctx
-
-  const result = await plugin.before(ctx)
-  if (!result) return ctx
-
-  const { query, ...init2 } = result
-  return deepMerge(ctx as object, {
-    init: init2,
-    args: { query },
-  } as object) as PluginBeforeContext<Fetcher>
-}
-
-async function applyAfter(
-  ctx: PluginAfterContext<Fetcher>,
-  plugin: Plugin<Fetcher>,
-): Promise<Response> {
-  if (!plugin.after) return ctx.res
-  const res = await plugin.after(ctx)
-  if (res) return res
-  return ctx.res
-}
-
-function stringifyEntries(obj: object) {
-  return Object.fromEntries(
-    Object.entries(obj).map(([key, val]) => [key, val.toString()]),
-  )
-}
-
-async function createBody(
-  actionConfig: BodyfullActionConfig<any>,
-  args: PossibleActionArgs | undefined,
-): Promise<{
-  body?: BodyInit
-  contentType?: string
-}> {
-  if (!args?.body || !actionConfig.body) {
-    return {}
-  }
-
-  const parsed = actionConfig.body?._transform?.(args.body) ??
-    actionConfig.body?.parse(args.body) ??
-    args.body
-
-  const bodySource = actionConfig.bodySource ?? "json"
-
-  const bodyData = typeof bodySource === "function"
-    ? await bodySource(parsed)
-    : parsed
-
-  if (isValidBody(bodyData)) {
-    return {
-      body: parsed,
-    }
-  }
-
-  switch (bodySource) {
-    case "json": {
-      return {
-        body: JSON.stringify(bodyData),
-        contentType: "application/json; charset=utf-8",
-      }
-    }
-    case "FormData": {
-      if (bodyData instanceof FormData) {
-        return {
-          body: bodyData,
-        }
-      }
-
-      const data = new FormData()
-
-      Object
-        .entries(bodyData)
-        .forEach(([key, value]) => data.append(key, (value as any).toString()))
-
-      return {
-        body: data,
-      }
-    }
-    case "URLSearchParameters": {
-      if (bodyData instanceof URLSearchParams) {
-        return {
-          body: bodyData,
-        }
-      }
-
-      const data = new URLSearchParams()
-
-      Object
-        .entries(bodyData)
-        .forEach(([key, value]) => data.append(key, (value as any).toString()))
-
-      return {
-        body: new URLSearchParams(data),
-      }
-    }
-    default: {
-      return {
-        body: bodyData,
-      }
-    }
-  }
-}
-
-async function parseData(
-  actionConfig: ActionConfig<any>,
-  res: Response,
-) {
-  if (!res.ok || (!actionConfig.data && !actionConfig.dataSource)) {
-    await res.body?.cancel()
-    return null
-  }
-
-  const dataSource = actionConfig.dataSource ?? "json"
-  const data = typeof dataSource === "function"
-    ? await dataSource(res)
-    : await res[dataSource]()
-
-  return actionConfig.data?._transform?.(data) ??
-    actionConfig.data?.parse(data) ?? data
-}
-
-function appendQuery(url: string, key: string, value: any) {
-  return url += `${url.includes("?") ? "&" : "?"}${key}=${value.toString()}`
-}
-
-function isValidBody(value: unknown) {
-  if (value === undefined) return true
-
-  if (typeof value === "object") {
-    if (value === null) return true
-    if ((value as any).constructor === Object) return false
-
-    const val = value as ArrayBufferView
-    const bufferCheck = val.buffer instanceof ArrayBuffer ||
-      val.buffer instanceof SharedArrayBuffer
-    const byteLengthCheck = typeof val.byteLength === "number"
-    const byteOffsetCheck = typeof val.byteOffset === "number"
-    if (bufferCheck && byteLengthCheck && byteOffsetCheck) return true
-  }
-
-  if (
-    typeof value === "string" ||
-    value instanceof URLSearchParams ||
-    value instanceof FormData ||
-    value instanceof Blob ||
-    value instanceof ArrayBuffer ||
-    value instanceof ReadableStream
-  ) {
-    return true
-  }
-
-  return false
 }
---
+++ tests/core/utils.test.ts
@@ -0,0 +1,75 @@
+import { assert } from "@std/assert/assert"
+import { isValidBody } from "../../src/utils.ts"
+import { assertFalse } from "@std/assert/assert-false"
+
+class ArrayBufferView_ArrayBuffer implements ArrayBufferView {
+  buffer: ArrayBufferLike = new ArrayBuffer(10)
+  byteLength: number = 10
+  byteOffset: number = 0
+}
+
+class ArrayBufferView_SharedArrayBuffer implements ArrayBufferView {
+  buffer: ArrayBufferLike = new SharedArrayBuffer(10)
+  byteLength: number = 10
+  byteOffset: number = 0
+}
+
+Deno.test("core - utils", async (t) => {
+  await t.step("isValidBody()", async (t) => {
+    await t.step("Should return true for string value", () => {
+      assert(isValidBody("string"))
+    })
+
+    await t.step("Should return true for URLSearchParams value", () => {
+      assert(isValidBody(new URLSearchParams()))
+    })
+
+    await t.step("Should return true for FormData value", () => {
+      assert(isValidBody(new FormData()))
+    })
+
+    await t.step("Should return true for Blob value", () => {
+      assert(isValidBody(new Blob()))
+    })
+
+    await t.step("Should return true for ArrayBuffer value", () => {
+      assert(isValidBody(new ArrayBuffer(10)))
+    })
+
+    await t.step("Should return true for Blob value", () => {
+      assert(isValidBody(new ReadableStream()))
+    })
+
+    await t.step(
+      "Should return true for ArrayBufferView value with ArrayBuffer",
+      () => {
+        assert(isValidBody(new ArrayBufferView_ArrayBuffer()))
+      },
+    )
+
+    await t.step(
+      "Should return true for ArrayBufferView value with SharedArrayBuffer",
+      () => {
+        assert(isValidBody(new ArrayBufferView_SharedArrayBuffer()))
+      },
+    )
+
+    await t.step("Should return false for plain object value", () => {
+      assertFalse(isValidBody({
+        foo: "foo",
+        bar: 10,
+        baz: {
+          riz: [1, 2, 3],
+        },
+      }))
+    })
+
+    await t.step("Should return false for number values", () => {
+      assertFalse(isValidBody(10))
+    })
+
+    await t.step("Should return false for array values", () => {
+      assertFalse(isValidBody([1, 2, 3]))
+    })
+  })
+})
