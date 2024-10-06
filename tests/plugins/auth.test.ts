c9fd9eb69a19928804568f75ad1adfdafb45d049
Oliver O'Loughlin <oliveroloughlin@hotmail.com>
2024-10-06 11:58:48 +0200

    chore: added test for api key auth

--- tests/plugins/auth.test.ts
+++ tests/plugins/auth.test.ts
@@ -3,6 +3,7 @@ import { assert, assertEquals } from "@std/assert"
 import { type AnythingData, createClient } from "../utils.ts"
 import { bearerAuth } from "../../src/plugins/auth/bearer.ts"
 import { schema } from "../../mod.ts"
+import { apiKeyAuth } from "../../src/plugins/auth/api-key.ts"
 
 const USERNAME = "user"
 const PASSWORD = "pass"
@@ -16,7 +17,39 @@ const basic = createClient({
   ],
 })
 
-const token = "78h23gbd3489hf7f2879fb7298f3"
+const token = crypto.randomUUID()
+const id = crypto.randomUUID()
+
+const apiKeyAuthHeadersDefault = createClient({
+  plugins: [apiKeyAuth({ apiKey: token })],
+})
+
+const apiKeyAuthQueryDefault = createClient({
+  plugins: [apiKeyAuth({ apiKey: token, strategy: "query" })],
+})
+
+const apiKeyAuthHeadersCustom = createClient({
+  plugins: [
+    apiKeyAuth({
+      apiKey: token,
+      apiKeyName: "X-KEY",
+      appId: id,
+      appIdName: "X-ID",
+    }),
+  ],
+})
+
+const apiKeyAuthQueryCustom = createClient({
+  plugins: [
+    apiKeyAuth({
+      apiKey: token,
+      apiKeyName: "X-KEY",
+      appId: id,
+      appIdName: "X-ID",
+      strategy: "query",
+    }),
+  ],
+})
 
 const staticBearerWithout = createClient({
   plugins: [bearerAuth({ token })],
@@ -92,6 +125,52 @@ Deno.test("plugins - auth", async (t) => {
       assertEquals(user, USERNAME)
       assertEquals(pass, PASSWORD)
     })
+  })
+
+  await t.step("api-key", async (t) => {
+    await t.step(
+      "Should set API key authentication in headers without app ID",
+      async () => {
+        const res = await apiKeyAuthHeadersDefault["/anything"].get()
+        assert(res.ok)
+
+        assertEquals(res.data.headers["X-Api-Key"], token)
+        assertEquals(res.data.headers["X-App-Id"], undefined)
+      },
+    )
+
+    await t.step(
+      "Should set custom API key authentication in headers with app ID",
+      async () => {
+        const res = await apiKeyAuthHeadersCustom["/anything"].get()
+        assert(res.ok)
+
+        assertEquals(res.data.headers["X-Key"], token)
+        assertEquals(res.data.headers["X-Id"], id)
+      },
+    )
+
+    await t.step(
+      "Should set API key authentication in query without app ID",
+      async () => {
+        const res = await apiKeyAuthQueryDefault["/anything"].get()
+        assert(res.ok)
+
+        assertEquals(res.data.args["X-API-KEY"], token)
+        assertEquals(res.data.args["X-APP-ID"], undefined)
+      },
+    )
+
+    await t.step(
+      "Should set custom API key authentication in query with app ID",
+      async () => {
+        const res = await apiKeyAuthQueryCustom["/anything"].get()
+        assert(res.ok)
+
+        assertEquals(res.data.args["X-KEY"], token)
+        assertEquals(res.data.args["X-ID"], id)
+      },
+    )
   })
 
   await t.step("bearer", async (t) => {
